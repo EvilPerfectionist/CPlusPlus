6dace929a8027a90ba3b4c977c35f1ff4a4efe72
EvilPerfectionist <17862701097@163.com>
2018-04-29 09:15:16 +0800

    Temporary_1.cpp

--- src/Temporary_1.cpp
+++ src/Temporary_1.cpp
@@ -14,9 +14,9 @@ int main()
     glewExperimental = GL_TRUE;
     glewInit();
 
-    Model model("/home/leon/Instrument_Pose_Estimation/Test_C", "nanosuit.obj");// Iron_Man_mark_6.dae  model_simplified.sdf
+    Model model("/home/leon/Instrument_Pose_Estimation/Test_C", "Iron_Man_mark_6.dae");// Iron_Man_mark_6.dae  model_simplified.sdf
 
-    //Model room("/home/leon/Instrument_Pose_Estimation/Test_C","room.dae", false);
+    Model room("/home/leon/Instrument_Pose_Estimation/Test_C","room.dae", false);
 
     // run the main loop
     bool running = true;
@@ -53,10 +53,14 @@ int main()
         cout << "press ENTER to run tracking, press SPACE to toggle first person view (use WASD-keys to move around)" << endl;
         while(!sf::Keyboard::isKeyPressed(sf::Keyboard::Return)) {
             model.updateViewMatrix(window);
-            model.render(img_camera, true, "color_simple");
+            room.renderer->ViewMatrix = model.renderer->ViewMatrix;
+            room.render(img_camera, true);
+            model.render(img_camera, false, "color_simple");
+            //room.Draw(true,"Model_Loading");
+            //model.Draw(true,"Model_Loading");
             window.display();
         }
-
+        
 
     }
 }
